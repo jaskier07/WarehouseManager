0d3efa7f5fc0fe14e9ae827ca81b5e3d07e0734c
jaskier07 <aleksanderkania96@gmail.com>
2019-11-05 18:12:40 +0100

    Initial commit for client

---
+++ Client/app/src/main/java/pl/kania/warehousemanagerclient/MainActivity.java
@@ -0,0 +1,21 @@
+package pl.kania.warehousemanagerclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.kania.warehousemanagerclient.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ Client/app/src/main/java/pl/kania/warehousemanagerclient/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package pl.kania.warehousemanagerclient.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
