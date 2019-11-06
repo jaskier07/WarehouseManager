ebfe3343b4e02195c6cdd7dc66f3a2a1c783238f
jaskier07 <aleksanderkania96@gmail.com>
2019-11-06 22:33:51 +0100

    All product parts in ProductAdapter

--- Client/app/src/main/java/pl/kania/warehousemanagerclient/ProductAdapter.java
+++ Client/app/src/main/java/pl/kania/warehousemanagerclient/ProductAdapter.java
@@ -6,15 +6,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.lang.reflect.Array;
 import java.util.List;
-
-import pl.kania.warehousemanagerclient.ui.main.ProductListFragment;
 
 public class ProductAdapter extends ArrayAdapter<Product> {
     public ProductAdapter(@NonNull Context context, @LayoutRes int resource, List<Product> objects) {
@@ -25,12 +23,29 @@ public class ProductAdapter extends ArrayAdapter<Product> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItemView = convertView;
-        if(listItemView == null) {
+        if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.product_list_element_fragment, parent, false);
         }
         Product product = getItem(position);
+
         EditText manufacturerValue = listItemView.findViewById(R.id.manufacturerValue);
         manufacturerValue.setText(product.getManufacturerName());
+        EditText modelValue = listItemView.findViewById(R.id.modelValue);
+        modelValue.setText(product.getModelName());
+        EditText priceValue = listItemView.findViewById(R.id.priceValue);
+        priceValue.setText(getNullSafeNumberValue(product.getPrice()));
+        TextView quantityValue = listItemView.findViewById(R.id.quantityValue);
+        quantityValue.setText(getNullSafeNumberValue(product.getQuantity()));
+        TextView idValue = listItemView.findViewById(R.id.idValue);
+        idValue.setText(getNullSafeNumberValue(product.getId()));
+
         return listItemView;
+    }
+
+    private String getNullSafeNumberValue(Number number) {
+        if (number == null) {
+            return "-";
+        }
+        return number.toString();
     }
 }
