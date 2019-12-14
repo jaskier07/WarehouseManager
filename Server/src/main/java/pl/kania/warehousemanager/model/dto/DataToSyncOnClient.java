dd44a7e600a3a1db4af73ee77c7067b8de07ff51
jaskier07 <aleksanderkania96@gmail.com>
2019-12-14 21:18:13 +0100

    Saved product ids in data to sync on client

--- Server/src/main/java/pl/kania/warehousemanager/model/dto/DataToSyncOnClient.java
+++ Server/src/main/java/pl/kania/warehousemanager/model/dto/DataToSyncOnClient.java
@@ -5,12 +5,15 @@ import lombok.Setter;
 import pl.kania.warehousemanager.model.db.Product;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Getter
 @Setter
-public class DataToSyncOnDevice {
+public class DataToSyncOnClient {
     private List<Long> removedIds = new ArrayList<>();
     private List<Product> newProducts = new ArrayList<>();
     private List<Product> updatedProducts = new ArrayList<>();
+    private Map<Long, Long> savedProductIdPerLocalId = new HashMap<>();
 }
